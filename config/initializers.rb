629264b253b0fe5ebff54d79a3411129ffb8d77c
Karim Tarek <karimmtarek@gmail.com>
2026-03-03 21:42:24 -0800

    Add GitHub Actions workflow for Pages deployment

--- config/initializers.rb
+++ config/initializers.rb
@@ -16,7 +16,7 @@
 
 Bridgetown.configure do |config|
   # The base hostname & protocol for your site, e.g. https://example.com
-  url ""
+  url "https://karimtarek.com"
 
   # Available options are `erb` (default), `serbea`, or `liquid`
   template_engine "erb"
